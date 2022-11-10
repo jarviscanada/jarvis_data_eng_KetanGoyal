31f1ecc1ea805969e9e657336011465c4814a136
Ketan Goyal <iamketangoyal@gmail.com>
2022-11-10 02:36:19 +0000

    modified ddl to remove foreign key

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,26 +1,28 @@
 
 
-CREATE TABLE if not exists PUBLIC.host_info
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info
   (
-     id                 SERIAL NOT NULL PRIMARY KEY AUTO_INCREMENT,
+     id                 BIGSERIAL NOT NULL PRIMARY KEY,
      hostname           VARCHAR NOT NULL UNIQUE,
      cpu_number         INT NOT NULL,
      cpu_architecture   VARCHAR(15) NOT NULL,
      cpu_model          VARCHAR(50) NOT NULL,
-     cpu_mhz            FLOAT(7,3) NOT NULL,
+     cpu_mhz            DECIMAL(7,3) NOT NULL,
      L2_cache           INT NOT NULL,
      total_mem          INT NOT NULL,
      timestamp          TIMESTAMP NOT NULL
   );
 
 
-  CREATE TABLE if not exists PUBLIC.host_usage
+  CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
     (
        timestamp      TIMESTAMP NOT NULL,
-       host_id        SERIAL NOT NULL FOREIGN key,
+       host_id        BIGSERIAL NOT NULL,
        memory_free    INT NOT NULL,
        cpu_idle       INT NOT NULL,
        cpu_kernel     INT NOT NULL,
        disk_io        INT NOT NULL,
        disk_available INT NOT NULL
+
     );
+
